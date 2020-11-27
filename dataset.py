368d84cda5f789b5014fe1b660dd58403d294abe
Ying <563520646@qq.com>
2020-11-27 08:51:29 +0800

    Update dateset.py

--- dataset.py
+++ dataset.py
@@ -3,11 +3,11 @@ from torch.utils.data import Dataset
 from torch.nn.utils.rnn import pad_sequence
 
 
-class LcccDataset(Dataset):
+class MyDataset(Dataset):
 
     def __init__(self, data, tokenizer, n_ctx, mmi=False, max_history=10):
         # mmi模型用回答预测问句，需要将对话逆序
-        self.data = data if not mmi else reversed(data)
+        self.data = data
         self.tokenizer = tokenizer
         self.max_length = n_ctx
         self.mmi = mmi
@@ -21,6 +21,7 @@ class LcccDataset(Dataset):
 
     def __getitem__(self, index):
         dialog = self.data[index][-2 * self.max_history:]
+        dialog = dialog if not self.mmi else reversed(dialog)
         return self.process(dialog)
 
     def process(self, dialog):
@@ -33,10 +34,10 @@ class LcccDataset(Dataset):
             dialog_ids.extend(sequence)
             dialog_ids.append(self.tokenizer.sep_token_id)
             # 处理token_type_ids，注意i从0开始
-            if not self.mmi:
-                token_type_ids.extend([self.speaker2 if i % 2 else self.speaker1] * len(sequence))
-            else:
+            if self.mmi:
                 token_type_ids.extend([self.speaker1 if i % 2 else self.speaker2] * len(sequence))
+            else:
+                token_type_ids.extend([self.speaker2 if i % 2 else self.speaker1] * len(sequence))
             token_type_ids.append(self.tokenizer.sep_token_id)
 
         # 截断超出序列最大长度(n_ctx)的部分
